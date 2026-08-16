385785451272f088345d40e3459c52123ac349e8
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2026-08-16 16:09:10 +0100

    Local dev: use webpack instead of Turbopack (Turbopack's Google Fonts loader was crashing dev server)

--- dev-server.cmd
+++ dev-server.cmd
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\dev\pmr-atlas
-"C:\Program Files\nodejs\node.exe" node_modules\next\dist\bin\next dev
+"C:\Program Files\nodejs\node.exe" node_modules\next\dist\bin\next dev --webpack
